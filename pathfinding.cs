68887ccbdef23ad774bfa7e6ab89e0b174e405fd
dreamkhkim <31675880+dreamkhkim@users.noreply.github.com>
2018-03-22 12:48:14 +0900

    Add files via upload

---
+++ pathfinding.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+    Grid grid;
+
+    void Update()
+    {
+        findPath(seeker.position, target.position);
+    }
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    void findPath(Vector3 startpos, Vector3 targetPos)
+    {
+        Node startnode = grid.NodeFromWorldPoint(startpos);
+        Node targetnode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startnode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fcost < currentNode.fcost || openSet[i].fcost == currentNode.fcost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetnode)
+            {
+                RetracePath(startnode, targetnode);
+                return;
+            }
+            foreach (Node neighbour in grid.GetNeighbours(currentNode))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+                int newMovementCostToNeighour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                if(newMovementCostToNeighour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newMovementCostToNeighour;
+                    neighbour.hCost = GetDistance(neighbour, targetnode);
+                    neighbour.parent = currentNode;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+        }
+    }
+
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while(currentNode !=startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+        grid.path = path;
+    }
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int disX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int disY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (disX > disY)
+            return 14 * disY + 10 * (disX - disY);
+        return 14 * disX + 10 * (disY - disX); 
+    }
+}
